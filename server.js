a0e0ce42feb8f9b558523f6c63b2fbb6054de251
tomas gallucci <tomigallucci@gmail.com>
2022-04-06 11:19:47 -0300

    add file

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const port = 3000;
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Hello world");
+})
+
+server.listen(port, ()=>{
+    console.log("Server listen on port", port)
+})
